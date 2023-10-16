e8208acc2a8031440939918d384175d95561c075
Leg3ndary <ben.zhou.2006@gmail.com>
2023-10-16 15:39:23 -0400

    Add basic page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Ubuntu } from "next/font/google";
 
-const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"]});
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 
-
 export default function Home() {
     return (
-        <main
-            className="flex min-h-screen flex-col items-center justify-between p-24"
-        >
-
+        <main className="flex min-h-screen flex-col items-center justify-between w-full">
+            <section className="flex flex-col items-center justify-center h-[400px] bg-gradient-to-r from-cyan-400 to-purple-500 w-full border-b-2 border-slate-500">
+                <h1 className="text-7xl font-bold text-center py-6">
+                    Abbey Park Arduino Competition
+                </h1>
+                <h3 className="text-3xl text-center text-slate-200">
+                    November 13th - December 4th
+                </h3>
+            </section>
         </main>
     );
 }
