5ee639a13f8e0fbfb03aace720fa2f94b57da8c3
ozzy <businessozzy27@gmail.com>
2023-11-07 15:41:11 -0500

    (made it more readable)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -163,7 +163,7 @@ export default function Home() {
                             </div>
                             <p className="text-xl lg:text-3xl">
                                 Awarded to the second best project submitted,
-                                win an Arduino R3!
+                                an Arduino R3!
                             </p>
                         </div>
                     </div>
